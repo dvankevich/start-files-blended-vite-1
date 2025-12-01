2e0649a601e0cc2933955edfbd21032c10969813
dvankevich <dvankevich@gmail.com>
2025-12-01 18:51:24 +0200

    task02 refactoring

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -4,30 +4,25 @@ import { GiTreeDoor } from 'react-icons/gi';
 import StatisticsItem from '../StatisticsItem/StatisticsItem';
 import style from './Statistics.module.css';
 
-const Statistics = ({ title, stats }) => {
-  const getIcon = title => {
-    switch (title) {
-      case 'Happy Customers':
-        return <FaRegThumbsUp size="36" />;
-      case 'Registered Members':
-        return <MdPeople size="36" />;
-      case 'Available Products':
-        return <MdOutlineProductionQuantityLimits size="36" />;
-      case 'Saved Trees':
-        return <GiTreeDoor size="36" />;
-      default:
-        return null;
-    }
-  };
+const icons = [
+  FaRegThumbsUp,
+  MdPeople,
+  MdOutlineProductionQuantityLimits,
+  GiTreeDoor,
+];
 
+const Statistics = ({ title, stats }) => {
   return (
     <>
       {title && <h3 className={style.title}>{title}</h3>}
       <ul className={style.list}>
-        {stats.map(stat => (
-          <li key={stat.id} className={style.item}>
-            {getIcon(stat.title)}
-            <StatisticsItem title={stat.title} total={stat.total} />
+        {stats.map((item, index) => (
+          <li key={item.id} className={style.item}>
+            <StatisticsItem
+              title={item.title}
+              total={item.total}
+              icon={icons[index]}
+            />
           </li>
         ))}
       </ul>
--- src/components/StatisticsItem/StatisticsItem.jsx
+++ src/components/StatisticsItem/StatisticsItem.jsx
@@ -1,8 +1,9 @@
 import style from './StatisticsItem.module.css';
 
-const StatisticsItem = ({ title, total }) => {
+const StatisticsItem = ({ title, total, icon: Icon }) => {
   return (
     <>
+      <Icon className={style.icon} />
       <span className={style.counter}>{total}</span>
       <p className={style.text}>{title}</p>
     </>
